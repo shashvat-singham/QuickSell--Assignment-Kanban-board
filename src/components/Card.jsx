9180a0b72e212c7645ed702ce3441fc0f908c84c
shashvat singham <78735607+shashvat-singham@users.noreply.github.com>
2023-11-14 05:21:51 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -20,8 +20,8 @@ const Card = ({ id, title, tags, status }) => {
 
         <div className="image">
           <img
-            src="https://quicksell.co/assets/logo/logo.png"
-            alt="QuickSell"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+            alt="User Image"
           />
 
           <div className="status"></div>
