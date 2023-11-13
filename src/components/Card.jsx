305899c59c64fe6d1ca478e0385db90be6c7fdfd
shashvat singham <78735607+shashvat-singham@users.noreply.github.com>
2023-11-14 05:28:28 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,7 +21,7 @@ const Card = ({ id, title, tags, status }) => {
         <div className="image">
           <img
             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
-            alt="User Image"
+            alt="User Name"
           />
 
           <div className="status"></div>
