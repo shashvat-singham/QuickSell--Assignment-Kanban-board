dea2c59e1b9eaf52ce697bd675457ac0cd37c25e
Shashvat Singham <shashvat.singham@gmail.com>
2023-11-14 04:54:48 +0530

    InitialCommit

---
+++ src/components/Card.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { BsExclamationSquareFill } from "react-icons/bs";
+import "../styles/Card.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+
+const dotStyle = {
+    color: 'silver', // Silver color
+    fontSize: '1em', // Adjust the size as needed
+    marginTop: '-3em', 
+  };
+
+const Card = ({ id, title, tags, status }) => {
+  return (
+    <div className="container">
+      <div className="cardHeading2" style={{ justifyContent: "space-between" }}>
+        <span style={{ textTransform: "uppercase", color: "darkgrey" }}>
+          {id}
+        </span>
+
+        <div className="image">
+          <img
+            src="https://quicksell.co/assets/logo/logo.png"
+            alt="QuickSell"
+          />
+
+          <div className="status"></div>
+        </div>
+      </div>
+
+      <div className="title">
+        <p>{title}</p>
+      </div>
+
+      <div className="tags">
+        <div className="tag">
+          <BsExclamationSquareFill />
+        </div>
+        {tags?.map((element, index) => {
+          return (
+            <div key={index} className="tag">
+              <span><FontAwesomeIcon icon={faCircle} style={dotStyle} /></span> {element}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
